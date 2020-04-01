61d787b43325f17bf724b3a423109e23be9ff546
Alexandru Vladutu <github@social.alessioalex.com>
2020-04-02 00:23:07 +0300

    created generic fetch fn

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
-
 import './styles.scss';
 
+import endpoint from './endpoint';
+
+const useFetch = url => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setResponse([]);
+    setError(null);
+
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        // console.log('response', response);
+        setLoading(false);
+        setResponse(response);
+      })
+      .catch(error => {
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
+
+  return [response, loading, error];
+};
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(`${endpoint}/characters`);
+  const characters = response ? response.characters : [];
 
   return (
     <div className="Application">
@@ -19,7 +46,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>

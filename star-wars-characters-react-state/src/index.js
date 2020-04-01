3d83447eacd18db841845a3864a9ef1ccf8c6dc5
Alexandru Vladutu <github@social.alessioalex.com>
2020-04-02 00:57:54 +0300

    back to the drawing board (pre-thunks)

--- star-wars-characters-react-state/src/index.js
+++ star-wars-characters-react-state/src/index.js
@@ -1,28 +1,26 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import './styles.scss';
-
 import endpoint from './endpoint';
 
-const initialState = {
-  result: null,
-  loading: true,
-  error: null,
-};
+import './styles.scss';
 
-const fetchReducer = (state, action) => {
-  if (action.type === 'LOADING') {
-    return initialState;
+const reducer = (state, action) => {
+  if (action.type === 'FETCHING') {
+    return {
+      characters: [],
+      loading: true,
+      error: null,
+    };
   }
 
   if (action.type === 'RESPONSE_COMPLETE') {
     return {
-      result: action.payload.response,
+      characters: action.payload.characters,
       loading: false,
       error: null,
     };
@@ -30,7 +28,7 @@ const fetchReducer = (state, action) => {
 
   if (action.type === 'ERROR') {
     return {
-      result: null,
+      characters: [],
       loading: false,
       error: action.payload.error,
     };
@@ -39,33 +37,15 @@ const fetchReducer = (state, action) => {
   return state;
 };
 
-const useFetch = url => {
-  const [state, dispatch] = useReducer(fetchReducer, initialState);
-
-  useEffect(() => {
-    dispatch({ type: 'LOADING ' });
-
-    const fetchUrl = async () => {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        dispatch({ type: 'RESPONSE_COMPLETE', payload: { response: data } });
-      } catch (error) {
-        dispatch({ type: error, payload: error });
-      }
-    };
-
-    fetchUrl();
-    // eslint-disable-next-line
-  }, []);
-
-  return state;
+const initialState = {
+  error: null,
+  loading: false,
+  characters: [],
 };
 
 const Application = () => {
-  const { result, loading, error } = useFetch(`${endpoint}/characters`);
-  const characters = result ? result.characters : [];
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { characters } = state;
 
   return (
     <div className="Application">
@@ -74,12 +54,8 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <CharacterList characters={characters} />
-          )}
-          {error && <p className="error">{error.message}</p>}
+          <button onClick={() => {}}>Fetch Characters</button>
+          <CharacterList characters={characters} />
         </section>
       </main>
     </div>
